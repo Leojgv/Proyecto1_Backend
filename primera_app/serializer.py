689d888c27145f58461c9869f8aeafffaf6e6075
Leonardo Gonzalez <gonzalezleonardo042@gmail.com>
2025-09-03 23:52:38 -0400

    serializer

---
+++ primera_app/serializer.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import programmer 
+from .models import Nacionalidad, Autor, Comuna, Direccion
+
+
+class Nacionalidad_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nacionalidad
+        fields = '__all__'
+
+class Autor_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = '__all__'
+
+class Comuna_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comuna
+        fields = '__all__'
+
+class Direccion_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Direccion
+        fields = '__all__'
