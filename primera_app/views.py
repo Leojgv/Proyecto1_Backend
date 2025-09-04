7720e60e28b9ee446e220801763d3138b413638c
Leonardo Gonzalez <gonzalezleonardo042@gmail.com>
2025-09-03 23:56:28 -0400

    serializer

--- primera_app/views.py
+++ primera_app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
+#from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import ProgrammerSerializer
+from .models import programmer
 # Create your views here.
