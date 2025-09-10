a0eb5440e40e284f46a6e4ef70a1b75d8cdfe771
Leonardo Gonzalez <gonzalezleonardo042@gmail.com>
2025-09-10 18:21:45 -0300

    creacion vistas

--- primera_app/views.py
+++ primera_app/views.py
@@ -1,5 +1,23 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from rest_framework import viewsets
-from .serializer import ProgrammerSerializer
-from .models import programmer
+from .serializer import Nacionalidad_Serializer, Comuna_Serializer, Autor_Serializer, Direccion_Serializer
+from .models import Nacionalidad, Comuna, Autor, Direccion
+
+
 # Create your views here.
+
+class ComunaViewSet(viewsets.ModelViewSet):
+    queryset = Comuna.objects.all()
+    serializer_class = Comuna_Serializer
+
+class NacionalidadViewSet(viewsets.ModelViewSet):
+    queryset = Nacionalidad.objects.all()
+    serializer_class = Nacionalidad_Serializer
+
+class AutorViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = Autor_Serializer
+
+class DireccionViewSet(viewsets.ModelViewSet):
+    queryset = Direccion.objects.all()
+    serializer_class = Direccion_Serializer
