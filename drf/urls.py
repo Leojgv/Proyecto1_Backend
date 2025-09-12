805b603e28bfbdf09aacb63a74d4b27f46fabe98
Leonardo Gonzalez <gonzalezleonardo042@gmail.com>
2025-09-12 20:37:08 -0300

    urls

--- drf/urls.py
+++ drf/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from primera_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
     path('primera_app/', include('primera_app.urls')),
 ]
